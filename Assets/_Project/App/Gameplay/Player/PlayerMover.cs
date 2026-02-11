fef1d22ab3a0c26944270b413087f5a9d3157525
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2026-02-11 16:17:22 +0200

    Fix input bug

--- Assets/_Project/App/Gameplay/Player/PlayerMover.cs
+++ Assets/_Project/App/Gameplay/Player/PlayerMover.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 public sealed class PlayerMover : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _smooth = 10f;
     [SerializeField] private float _minX = -3.5f;
     [SerializeField] private float _maxX = 3.5f;
 
-    private void Awake()
-    {
-        Debug.Log($"PlayerMover Awake on: {gameObject.name}", this);
-    }
+    private float _currentInput;
 
     public void Move(float inputX)
     {
-        Debug.Log($"input={inputX}, speed={_speed}, delta={inputX * _speed * Time.deltaTime}");
+        _currentInput = Mathf.Lerp(_currentInput, inputX, _smooth * Time.deltaTime);
+
         Vector3 pos = transform.position;
         pos.x += inputX * _speed * Time.deltaTime;
         pos.x = Mathf.Clamp(pos.x, _minX, _maxX);

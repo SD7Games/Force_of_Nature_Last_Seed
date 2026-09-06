a2c9d1ef4dcc6d3a61036415f7d057eb213bf659
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2026-09-06 13:47:40 +0300

    refactor: split reward popup action controls

--- Assets/_Project/Presentation/UI/Rewards/RewardPopupActionControls.cs
+++ Assets/_Project/Presentation/UI/Rewards/RewardPopupActionControls.cs
@@ -9,24 +9,11 @@ public sealed class RewardPopupActionControls
     private readonly Button _rerollButton;
     private readonly Button _adRerollButton;
     private readonly Button _takeAllButton;
-    private readonly TMP_Text _rerollAttemptsText;
-    private readonly TMP_Text _adRerollAttemptsText;
-    private readonly TMP_Text _takeAllAttemptsText;
-    private readonly TMP_Text _guaranteeText;
-    private readonly TMP_Text _adRerollGuaranteeText;
-    private readonly TextSettings _textSettings;
-    private readonly float _singleActionButtonAnchoredX;
     private readonly Action _onRerollClicked;
     private readonly Action _onAdRerollClicked;
     private readonly Action _onTakeAllClicked;
-
-    private RewardPopupRectTransformState[] _buttonStates;
-    private RewardPopupRectTransformState _rerollAttemptsTextState;
-    private RewardPopupRectTransformState _adRerollAttemptsTextState;
-    private RewardPopupRectTransformState _takeAllAttemptsTextState;
-    private RewardPopupState _currentState;
-    private bool _hasCurrentState;
-    private bool _hasCachedLayoutState;
+    private readonly RewardPopupActionStatePresenter _statePresenter;
+    private readonly RewardPopupActionLayoutAnimator _layoutAnimator;
 
     public RewardPopupActionControls(
         Button rerollButton,
@@ -46,16 +33,27 @@ public sealed class RewardPopupActionControls
         _rerollButton = rerollButton;
         _adRerollButton = adRerollButton;
         _takeAllButton = takeAllButton;
-        _rerollAttemptsText = rerollAttemptsText;
-        _adRerollAttemptsText = adRerollAttemptsText;
-        _takeAllAttemptsText = takeAllAttemptsText;
-        _guaranteeText = guaranteeText;
-        _adRerollGuaranteeText = adRerollGuaranteeText;
-        _textSettings = textSettings;
-        _singleActionButtonAnchoredX = singleActionButtonAnchoredX;
         _onRerollClicked = onRerollClicked;
         _onAdRerollClicked = onAdRerollClicked;
         _onTakeAllClicked = onTakeAllClicked;
+        _statePresenter = new RewardPopupActionStatePresenter(
+            rerollButton,
+            adRerollButton,
+            takeAllButton,
+            rerollAttemptsText,
+            adRerollAttemptsText,
+            takeAllAttemptsText,
+            guaranteeText,
+            adRerollGuaranteeText,
+            textSettings);
+        _layoutAnimator = new RewardPopupActionLayoutAnimator(
+            rerollButton,
+            adRerollButton,
+            takeAllButton,
+            rerollAttemptsText,
+            adRerollAttemptsText,
+            takeAllAttemptsText,
+            singleActionButtonAnchoredX);
     }
 
     public void Subscribe()
@@ -89,50 +87,21 @@ public sealed class RewardPopupActionControls
         bool interactable,
         bool resetLayout)
     {
-        _currentState = state;
-        _hasCurrentState = true;
-
-        ApplyRerollButtonMode(state);
-        ApplyTakeAllButtonMode(state);
-        ApplyAttemptsText(_rerollAttemptsText, state.FreeRerollAttemptsLeft);
-        ApplyAttemptsText(_adRerollAttemptsText, state.AdRerollAttemptsLeft);
-        ApplyAttemptsText(_takeAllAttemptsText, state.TakeAllAttemptsLeft);
-
-        ApplyGuaranteeText(_guaranteeText, state.GuaranteeRarity);
-        ApplyAdGuaranteeText(_adRerollGuaranteeText, state.AdRerollGuaranteeRarity);
-
-        SetInteractable(interactable);
+        _statePresenter.Apply(state, interactable);
+        _layoutAnimator.SetState(state);
 
         if (resetLayout)
-            ResetLayoutIfCached();
+            _layoutAnimator.ResetIfCached();
     }
 
     public void SetInteractable(bool enabled)
     {
-        bool hasState = _hasCurrentState;
-
-        if (_rerollButton != null)
-            _rerollButton.interactable = enabled && hasState && _currentState.CanFreeReroll;
-
-        if (_adRerollButton != null)
-            _adRerollButton.interactable = enabled && hasState && _currentState.CanAdReroll;
-
-        if (_takeAllButton != null)
-            _takeAllButton.interactable = enabled && hasState && _currentState.CanTakeAll;
+        _statePresenter.SetInteractable(enabled);
     }
 
     public void PrepareForEnter(float yOffset)
     {
-        EnsureLayoutCached();
-
-        for (int i = 0; i < _buttonStates.Length; i++)
-        {
-            _buttonStates[i].Kill();
-            _buttonStates[i].Prepare(
-                GetActionButtonTargetPosition(_buttonStates[i]),
-                yOffset,
-                0.98f);
-        }
+        _layoutAnimator.PrepareForEnter(yOffset);
     }
 
     public void InsertEnterTweens(
@@ -140,24 +109,7 @@ public sealed class RewardPopupActionControls
         float startTime,
         float duration)
     {
-        EnsureLayoutCached();
-
-        int activeIndex = 0;
-
-        for (int i = 0; i < _buttonStates.Length; i++)
-        {
-            if (!_buttonStates[i].IsActive)
-                continue;
-
-            sequence.Insert(
-                startTime + activeIndex * 0.035f,
-                _buttonStates[i].CreateEnterTween(
-                    GetActionButtonTargetPosition(_buttonStates[i]),
-                    duration,
-                    Ease.OutCubic,
-                    Ease.OutBack));
-            activeIndex++;
-        }
+        _layoutAnimator.InsertEnterTweens(sequence, startTime, duration);
     }
 
     public void InsertExitTweens(
@@ -167,183 +119,17 @@ public sealed class RewardPopupActionControls
         float duration,
         Ease exitEase)
     {
-        EnsureLayoutCached();
-
-        int activeIndex = 0;
-
-        for (int i = _buttonStates.Length - 1; i >= 0; i--)
-        {
-            if (!_buttonStates[i].IsActive)
-                continue;
-
-            sequence.Insert(
-                startTime + activeIndex * 0.035f,
-                _buttonStates[i].CreateExitTween(
-                    GetActionButtonTargetPosition(_buttonStates[i]),
-                    yOffset,
-                    0.98f,
-                    duration,
-                    exitEase,
-                    exitEase));
-            activeIndex++;
-        }
+        _layoutAnimator.InsertExitTweens(sequence, startTime, yOffset, duration, exitEase);
     }
 
     public void ResetLayout()
     {
-        EnsureLayoutCached();
-        ResetCachedLayout();
-    }
-
-    private void ResetLayoutIfCached()
-    {
-        if (!_hasCachedLayoutState)
-            return;
-
-        ResetCachedLayout();
-    }
-
-    private void ResetCachedLayout()
-    {
-        for (int i = 0; i < _buttonStates.Length; i++)
-            _buttonStates[i].Reset(GetActionButtonTargetPosition(_buttonStates[i]));
-
-        _rerollAttemptsTextState.Reset();
-        _adRerollAttemptsTextState.Reset();
-        _takeAllAttemptsTextState.Reset();
+        _layoutAnimator.Reset();
     }
 
     public void KillAnimations()
     {
-        if (_buttonStates == null)
-            return;
-
-        for (int i = 0; i < _buttonStates.Length; i++)
-            _buttonStates[i].Kill();
-    }
-
-    private void EnsureLayoutCached()
-    {
-        if (_hasCachedLayoutState)
-            return;
-
-        _buttonStates = CreateButtonStates();
-        _rerollAttemptsTextState = new RewardPopupRectTransformState(GetRectTransform(_rerollAttemptsText));
-        _adRerollAttemptsTextState = new RewardPopupRectTransformState(GetRectTransform(_adRerollAttemptsText));
-        _takeAllAttemptsTextState = new RewardPopupRectTransformState(GetRectTransform(_takeAllAttemptsText));
-        _hasCachedLayoutState = true;
-    }
-
-    private RewardPopupRectTransformState[] CreateButtonStates()
-    {
-        RectTransform reroll = GetRectTransform(_rerollButton);
-        RectTransform adReroll = GetRectTransform(_adRerollButton);
-        RectTransform takeAll = GetRectTransform(_takeAllButton);
-
-        int count = 0;
-
-        if (reroll != null)
-            count++;
-
-        if (adReroll != null)
-            count++;
-
-        if (takeAll != null)
-            count++;
-
-        RewardPopupRectTransformState[] states = new RewardPopupRectTransformState[count];
-        int index = 0;
-
-        if (reroll != null)
-            states[index++] = new RewardPopupRectTransformState(reroll);
-
-        if (adReroll != null)
-            states[index++] = new RewardPopupRectTransformState(adReroll);
-
-        if (takeAll != null)
-            states[index] = new RewardPopupRectTransformState(takeAll);
-
-        return states;
-    }
-
-    private Vector2 GetActionButtonTargetPosition(RewardPopupRectTransformState state)
-    {
-        if (!ShouldUseSingleActionLayout())
-            return state.BaseAnchoredPosition;
-
-        RectTransform activeReroll = _currentState.UseFreeRerollButton
-            ? GetRectTransform(_rerollButton)
-            : GetRectTransform(_adRerollButton);
-
-        if (state.RectTransform != activeReroll)
-            return state.BaseAnchoredPosition;
-
-        return new Vector2(
-            _singleActionButtonAnchoredX,
-            state.BaseAnchoredPosition.y);
-    }
-
-    private bool ShouldUseSingleActionLayout()
-    {
-        return _hasCurrentState && !_currentState.UseTakeAllButton;
-    }
-
-    private void ApplyRerollButtonMode(RewardPopupState state)
-    {
-        bool showFreeReroll = state.UseFreeRerollButton;
-
-        if (_rerollButton != null)
-            _rerollButton.gameObject.SetActive(showFreeReroll);
-
-        if (_adRerollButton != null)
-            _adRerollButton.gameObject.SetActive(!showFreeReroll);
-    }
-
-    private void ApplyTakeAllButtonMode(RewardPopupState state)
-    {
-        bool showTakeAll = state.UseTakeAllButton;
-
-        if (_takeAllButton != null)
-            _takeAllButton.gameObject.SetActive(showTakeAll);
-
-        if (_takeAllAttemptsText != null)
-            _takeAllAttemptsText.gameObject.SetActive(showTakeAll);
-    }
-
-    private void ApplyGuaranteeText(TMP_Text text, RewardRarity rarity)
-    {
-        if (text == null)
-            return;
-
-        text.text = RewardTextFormatter.FormatRarityLine(
-            _textSettings.GuaranteeFormat,
-            rarity,
-            _textSettings.CommonRarityColor,
-            _textSettings.RareRarityColor,
-            _textSettings.LegendaryRarityColor);
-    }
-
-    private void ApplyAdGuaranteeText(TMP_Text text, RewardRarity rarity)
-    {
-        if (text == null)
-            return;
-
-        text.text = RewardTextFormatter.FormatRarityLine(
-            _textSettings.AdGuaranteeFormat,
-            rarity,
-            _textSettings.CommonRarityColor,
-            _textSettings.RareRarityColor,
-            _textSettings.LegendaryRarityColor,
-            _textSettings.NumberColor);
-    }
-
-    private void ApplyAttemptsText(TMP_Text text, int attemptsLeft)
-    {
-        if (text == null)
-            return;
-
-        string value = string.Format(_textSettings.AttemptsFormat, Mathf.Max(0, attemptsLeft));
-        text.text = RewardTextFormatter.HighlightAttempts(value, _textSettings.NumberColor);
+        _layoutAnimator.KillAnimations();
     }
 
     private void HandleRerollClicked()
@@ -359,20 +145,6 @@ public sealed class RewardPopupActionControls
     private void HandleTakeAllClicked()
     {
         _onTakeAllClicked?.Invoke();
-    }
-
-    private static RectTransform GetRectTransform(Button button)
-    {
-        return button != null
-            ? button.transform as RectTransform
-            : null;
-    }
-
-    private static RectTransform GetRectTransform(TMP_Text text)
-    {
-        return text != null
-            ? text.transform as RectTransform
-            : null;
     }
 
     public readonly struct TextSettings
